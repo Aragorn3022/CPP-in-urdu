89c783b4db9c94d5f62b952e5d57a1c308331d23
Aswad <ahmad@ASWADAHMAD>
2023-06-10 21:30:55 +0500

    Add project files.

---
+++ CPP in urdu/Header1.h
@@ -0,0 +1,18 @@
+﻿#pragma region Defines
+#include<iostream>
+
+#define استعمال using namespace std;
+#define اگر if
+#define جبتک while
+#define کیلیے for 
+#define شماریہ int 
+#define شروع main()
+#define داخله cin >>
+#define باہر cout <<
+#define _باہر wcout <<
+#define اختتام endl;
+//#define اختیار 
+
+
+#pragma endregion
+
